d890c31f5270500a9f017e5d2e1f94e2aff0dd52
Eduardo Costa <eduhcosta@live.com>
2021-07-27 14:28:13 -0300

    Add caching performance like a service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// Using Workbox
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
@@ -27,5 +31,18 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({ template: './public/index.html' }),
+        new InjectManifest({
+            swSrc: path.join(process.cwd(), '/src/service-worker.js'),
+            swDest: 'service-worker.js',
+            chunks: ['*.chunk.js'],
+            exclude: [
+                /\.map$/,
+                /manifest$/,
+                /\.htaccess$/,
+                /service-worker\.js$/,
+                /sw\.js$/,
+            ],
+            include: [path.resolve(process.cwd(), 'dist')],
+        })
     ],
 }
